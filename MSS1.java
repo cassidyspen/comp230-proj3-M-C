a6341cdc42217338dc0c249a896aca36b9dc4681
cassidyspen <cassidyspencer@sandiego.edu>
2022-03-30 20:48:08 -0700

    mss2

---
+++ MSS1.java
@@ -0,0 +1,17 @@
+/*comments :) */
+public class MSS1
+{
+	public static void main(String[] arg)
+	{
+		//decalare variables
+		Scanner sc = new Scanner(System.in);
+
+		//get input from the user
+		System.out.println("Enter the name of the file");
+		String filename = sc.nextLine();
+
+		//open file connection
+		BufferedReader br = new Filereader
+	}
+}
+
