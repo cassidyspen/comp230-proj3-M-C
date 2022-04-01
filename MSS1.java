1fd99eadd47d6739f9d9e37347a32e0f4b54fce9
cassidyspen <cassidyspencer@sandiego.edu>
2022-03-31 19:13:06 -0700

    from atom

--- MSS1.java
+++ MSS1.java
@@ -1,4 +1,6 @@
 /*comments :) */
+import java.util.Scanner;
+
 public class MSS1
 {
 	public static void main(String[] arg)
@@ -10,16 +12,34 @@ public class MSS1
 		System.out.println("Enter a sequence");
 		String sequence = sc.nextLine();
 		String[] strArr = sequence.split(",");
-		
+
 		int[] intArr = new int[strArr.length];
-		
+
 		for(int i=0; i<strArr.length; i++)
 		{
 			intArr[i] = Integer.parseInt(strArr[i]);
 		}
+		int answer = mss1alg(intArr);
+		System.out.println("\n"+answer);
 
 
-		
 	}
-}
+	/*method for mss1*/
+	public static int mss1alg(int[] arr)
+	{
+		int sum = 0,maxSum = 0;
+		for(int i=0; i<arr.length; i++)
+		{
+			for(int j=i; i<arr.length; j++)
+			{
+				sum = 0;
+				for(int k=i; k<=j; k++)
+					sum += arr[k];
+				if (sum > maxSum)
+					maxSum = sum;
+			}
+		}
+		return maxSum;
+	}
 
+}
