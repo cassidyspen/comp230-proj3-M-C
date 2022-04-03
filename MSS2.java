649704850c495c9e04ee7f28d3e3b392ea39cd93
cassidyspen <cassidyspencer@sandiego.edu>
2022-04-02 22:14:21 -0700

    added switch statement

--- MSS2.java
+++ MSS2.java
@@ -9,21 +9,34 @@ public class MSS2 {
 		//reading in the file
 		try
 		{
+			//declare variables
 			String[] strArr;
 			//int[] intArray;
-			
+		//	int time1 = 0, time2 = 0, time3 = 0, time4 = 0, firstTime = 0, lastTime = 0;
+
 			//TODO change file input to variable
 			Scanner scan = new Scanner(System.in);
 			System.out.println("What file would you like to use?");
 			String user_input = scan.nextLine();
 			
+			//display menu
 			System.out.println("\nYour menu, madam/sir (please enter your number selection)\n");
-			System.out.println("Item # \t Name \t Cost");
+			System.out.print("Item # \t Name \t Cost\n"
+							  +"---------------------------------");
 			System.out.println("\n1.     \t MSS1 \t Muy Expensive");
 			System.out.println("2.     \t MSS2 \t Expensive");
 			System.out.println("3.     \t MSS3 \t A lil Expensive");
 			System.out.println("4.     \t MSS4 \t CHEAP");
-			System.out.println("5.     \t ALL  \t Oooh you fancy");
+			System.out.println("5.     \t ALL  \t Oooh you fancy\n");
+
+			//user input
+			int choice = scan.nextInt();
+			while(choice<1 || choice>5)
+			{
+				System.out.println("\nError: Please enter 1 to 5");
+				choice = scan.nextInt();
+			}
+
 
 			input = new BufferedReader(new FileReader(user_input));
 			while((inputLine = input.readLine()) != null) {
@@ -35,14 +48,34 @@ public class MSS2 {
 				for(int i=0; i<strArr.length; i++)
 					intArray[i] = Integer.parseInt(strArr[i]);
 			
-			
-				int mss1ans = mss1alg(intArray);
-				int mss2ans = mss2alg(intArray);
-				int mss3ans = mss3alg(intArray, 0, intArray.length-1);
-				int mss4ans = mss4alg(intArray);
+			System.out.println();
+			switch(choice)
+			{
+				case 1:
+					runAlgs(intArray,choice);
+					break;
+				case 2:
+					runAlgs(intArray,choice);
+					break;
+				case 3:
+					runAlgs(intArray,choice);
+					break;
+				case 4:
+					runAlgs(intArray,choice);
+					break;
+				default:
+					System.out.print("MSS1:\t");
+					runAlgs(intArray,1);
+					System.out.print("MSS2:\t");
+					runAlgs(intArray,2);
+					System.out.print("MSS3:\t");
+					runAlgs(intArray,3);
+					System.out.print("MSS4:\t");
+					runAlgs(intArray,4);
+			}
 
-				System.out.println("\nMSS1 answer: "+mss1ans+"\nMSS2 answer: "+mss2ans
-									+ "\nMSS3 answer: " +mss3ans+"\nMSS4 answer: "+mss4ans);
+			//	System.out.println("\nMSS1 answer: "+mss1ans+"\nMSS2 answer: "+mss2ans
+			//						+ "\nMSS3 answer: " +mss3ans+"\nMSS4 answer: "+mss4ans);
 			}
 
 
@@ -59,6 +92,28 @@ public class MSS2 {
 		} // end catch
 		
 		
+	}
+	public static void runAlgs(int[] intArray,int alg)
+	{
+		long firstTime = System.nanoTime();
+		int answer = 0;
+		//run alg
+		if(alg==1)
+			answer = mss1alg(intArray);
+		else if(alg==2)
+			answer = mss2alg(intArray);
+		else if(alg==3)
+			answer = mss3alg(intArray, 0, intArray.length-1);
+		else
+			answer = mss4alg(intArray);
+
+		//get time
+		long lastTime = System.nanoTime();
+		long time = lastTime - firstTime;
+
+		//display to user
+		System.out.println("Answer: "+answer+"\tTime: "+time);
+
 	}
 	/*method for mss1*/
      public static int mss1alg(int[] arr)
